dfd309de63b34ec8c73e9fba3287cbdba3bd816f
benish14 <pjenishp@gmail.com>
2026-05-01 18:58:01 +0530

    Initial backend commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -1,5 +1,4 @@
-import os
-from pathlib import Path
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -32,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sb(iuq9c)lpqt-pu0$7h@)ag0)e-5xhxv2sq96q%!pt232!=(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -97,24 +96,17 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
-
- 'default': {
-
-  'ENGINE': 'django.db.backends.mysql',
-
-  'NAME': BASE_DIR / 'db.sqlite3',
-
-  'USER': 'root',
-
-  'PASSWORD': 'pondevi@8675',
-
-  'HOST': 'localhost',
-
-  'PORT': '3306',
-
- }
-
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': os.getenv("DB_NAME"),
+    'USER': os.getenv("DB_USER"),
+    'PASSWORD': os.getenv("DB_PASSWORD"),
+    'HOST': os.getenv("DB_HOST"),
+    'PORT': '3306',
+  }
 }
 
 
@@ -163,3 +155,6 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+import os
+import pymysql
+pymysql.install_as_MySQLdb()
